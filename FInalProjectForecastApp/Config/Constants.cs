85bbc2f41163de99c88cf3f6bcb19f311063f909
Baruc Gomez <113378032+Bdotgo17@users.noreply.github.com>
2022-10-07 16:24:47 -0500

    intial commit

---
+++ FInalProjectForecastApp/Config/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForecastApp.Config
+{
+    public static class Constants
+    {
+        public const string OPEN_WEATHER_APPID = "14645bd5316c7fe0cf4d197b2c1d0c96";
+    }
+}
+
